a2b2db284e140442913567ca831ed882ee5e81c0
Muhammad Muneeb <muneebn048@gmail.com>
2025-11-30 21:59:29 +0500

    Update project files

--- my-app/eslint.config.js
+++ my-app/eslint.config.js
@@ -3,14 +3,15 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default [
-  { ignores: ['dist'] },
+export default defineConfig([
+  globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommended,
+      tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
@@ -19,4 +20,4 @@ export default [
       globals: globals.browser,
     },
   },
-]
+])
